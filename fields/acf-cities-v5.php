abb6cd5ac8ab19dcb2dbc6b150ee3e2c73ad1837
Diogenes Oliveira Junior <contato@diogenesjunior.com.br>
2024-01-28 18:04:40 -0300

    Update acf-cities-v5.php
    Remoção da API externa, aplicando um arquivo local JSON, e remoção do campo país (country)

--- fields/acf-cities-v5.php
+++ fields/acf-cities-v5.php
@@ -54,44 +54,26 @@ class acf_field_cities extends acf_field {
         ?>
         <ul class="acf-hl" data-cols="3">
             <li>
-                <select name="<?php echo esc_attr($field['name']) ?>[country]" data-select="countries" value="<?php echo isset($field['value']['country']) ? esc_attr($field['value']['country']) : ''; ?>">
-                    <option value=""><?php _e('País', 'acf-cities'); ?></option>
-                    <?php foreach($field['countries'] as $country): ?>
-                    <option value="<?php echo $country->Sigla; ?>" <?php echo ($country->Sigla === $field['value']['country']) ? 'selected' : '' ?>><?php echo $country->Pais; ?></option>
+                <select name="<?php echo esc_attr($field['name']) ?>[state]" data-select="states">
+                    <option value=""><?php _e('Estado', 'acf-cities'); ?></option>
+                    <?php foreach ($field['estados'] as $estado): ?>
+                        <option value="<?php echo esc_attr($estado['sigla']); ?>" <?php selected($estado['sigla'], $field['value']['state']); ?>>
+                            <?php echo esc_html($estado['nome']); ?>
+                        </option>
                     <?php endforeach; ?>
                 </select>
             </li>
-
+    
             <li>
-                <select name="<?php echo esc_attr($field['name']) ?>[state]" data-select="states" data-value="<?php echo isset($field['value']['state']) ? esc_attr($field['value']['state']) : ''; ?>" <?php echo (isset($field['value']['custom-state']) && $field['value']['custom-state'] == 1) ? 'style="display: none"' : ''; ?>>
-                    <option value=""><?php _e('Estado', 'acf-cities'); ?></option>
-                </select>
-
-                <input <?php echo (isset($field['value']['custom-state']) && $field['value']['custom-state'] == 1) ? 'name="'. esc_attr($field['name']) .'[state]"' : ''; ?> type="text" value="<?php echo esc_attr($field['value']['state']) ?>" placeholder="Digite o estado" class="state-input" <?php echo (!isset($field['value']['custom-state']) || $field['value']['custom-state'] == 0) ? 'style="display: none"' : ''; ?>>
-
-                <label>
-                    <input type="hidden" name="<?php echo esc_attr($field['name']) ?>[custom-state]" value="0">
-                    <input type="checkbox" name="<?php echo esc_attr($field['name']) ?>[custom-state]" <?php if (isset($field['value']['custom-state'])) checked($field['value']['custom-state'], 1); ?> value="1" class="custom-state-toggler">
-                    Não encontrei o estado
-                </label>
-            </li>
-
-            <li>
-                <select name="<?php echo esc_attr($field['name']) ?>[city]" data-select="cities" data-value="<?php echo isset($field['value']['city']) ? esc_attr($field['value']['city']) : ''; ?>" <?php echo (isset($field['value']['custom-city']) && $field['value']['custom-city'] == 1) ? 'style="display: none"' : ''; ?>>
+                <select name="<?php echo esc_attr($field['name']) ?>[city]" data-select="cities">
                     <option value=""><?php _e('Cidade', 'acf-cities'); ?></option>
+                    <!-- As cidades serão carregadas via JavaScript -->
                 </select>
-
-                <input <?php echo (isset($field['value']['custom-city']) && $field['value']['custom-city'] == 1) ? 'name="'. esc_attr($field['name']) .'[city]"' : ''; ?> type="text" value="<?php echo esc_attr($field['value']['city']) ?>" placeholder="Digite a cidade" class="city-input" <?php echo (!isset($field['value']['custom-city']) || $field['value']['custom-city'] == 0) ? 'style="display: none"' : ''; ?>>
-
-                <label>
-                    <input type="hidden" name="<?php echo esc_attr($field['name']) ?>[custom-city]" value="0">
-                    <input type="checkbox" name="<?php echo esc_attr($field['name']) ?>[custom-city]" <?php if (isset($field['value']['custom-city'])) checked($field['value']['custom-city'], 1); ?> value="1" class="custom-city-toggler">
-                    Não encontrei a cidade
-                </label>
             </li>
         </ul>
         <?php
     }
+    
 
     /*
     *  input_admin_enqueue_scripts()
@@ -129,25 +111,21 @@ class acf_field_cities extends acf_field {
     *  @return  $field
     */
     function load_field($field) {
-        if (($countries = get_transient('acf_cities_countries')) === false) {
-            $url = 'http://api.londrinaweb.com.br/PUC/Paisesv2/0/1000';
-
-            $response = wp_remote_get($url);
-
-            // Return nothing if the request returns an error
-            if (!is_wp_error($response)) {
-                $body = wp_remote_retrieve_body($response);
-                $data = json_decode($body);
-                $countries = $data;
-
-                set_transient('acf_cities_countries', $countries, 10 * MINUTE_IN_SECONDS);
-            }
+        $file_path = WP_PLUGIN_DIR . '/acf-cities-master/data/estados-cidades.json';
+    
+        if (file_exists($file_path)) {
+            $data = file_get_contents($file_path);
+            $estados = json_decode($data, true)['estados'];
+        } else {
+            $estados = array();
         }
-
-        $field['countries'] = $countries;
-
+    
+        $field['estados'] = $estados;
+    
         return $field;
     }
+    
+    
 }
 
 // initialize
